40598aa82f17c61eb05e6f854328d654dde5cb0e
Harsh Varlekar <harsh29varlekar@gmail.com>
2025-01-01 18:12:25 +0530

    03-Fix rubocop issue and set up basic devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'pages#index'
+  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
